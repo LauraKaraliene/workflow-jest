59e0351b96ee44311a8f7a686e4a00172b13a335
Laura Karaliene <Laura.Karaliene@stud.noroff.no>
2024-04-01 18:52:14 +0200

    updated package.json with test-e2e-cli' git status clear

--- cypress/e2e/3-testing/no.wikipedia.cy.js
+++ cypress/e2e/3-testing/no.wikipedia.cy.js
@@ -1,7 +1,9 @@
 describe("no.wikipedia.org", () => {
-    it("can search for Noroff", () => {
+    it("can search for Norway", () => {
       cy.visit("https://no.wikipedia.org");
-      cy.get("input#searchInput").type("Noroff{enter}", { delay: 500 });
-      cy.get('h1').contains("Noroff")
+      cy.get("input#searchInput").type("Norway{enter}", { delay: 500 });
+      cy.url().should('include', '/wiki/Norway');
+      cy.get('h1').should('contain', 'Norway');
+      //cy.get('p').contains("Norway");
     })
   })
