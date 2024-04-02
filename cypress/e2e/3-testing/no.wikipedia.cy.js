7a99f95978819728e424d471c180ca2a42c0b962
Laura Karaliene <Laura.Karaliene@stud.noroff.no>
2024-04-02 19:39:27 +0200

    new branch automates-testing created

--- cypress/e2e/3-testing/no.wikipedia.cy.js
+++ cypress/e2e/3-testing/no.wikipedia.cy.js
@@ -2,6 +2,7 @@ describe("no.wikipedia.org", () => {
     it("can search for Norway", () => {
       cy.visit("https://no.wikipedia.org");
       cy.get("input#searchInput").type("Norway{enter}", { delay: 500 });
+      cy.wait(10000);
       cy.url().should('include', '/wiki/Norway');
       cy.get('h1').should('contain', 'Norway');
       //cy.get('p').contains("Norway");
