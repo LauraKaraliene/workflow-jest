5c5b48bf7ff6ec1a47047bb49304d2346ad98517
Laura Karaliene <Laura.Karaliene@stud.noroff.no>
2024-04-06 17:52:26 +0200

    code update to pass a test

--- cypress/e2e/3-testing/no.wikipedia.cy.js
+++ cypress/e2e/3-testing/no.wikipedia.cy.js
@@ -1,9 +1,9 @@
-describe("no.wikipedia.org", () => {
-    it("can search for Norway", () => {
-      cy.visit("https://no.wikipedia.org");
-      cy.get("input#searchInput").type("Norway{enter}", { delay: 500 });
-      cy.url().should('include', '/wiki/Norway');
-      cy.get('h1').should('contain', 'Norway');
+describe("en.wikipedia.org", () => {
+    it("can search for Oslo", () => {
+      cy.visit("https://en.wikipedia.org");
+      cy.get("input#searchInput").type("Oslo{enter}", { delay: 500 });
+      cy.url().should('include', '/wiki/Oslo');
+      cy.get('h1').should('contain', 'Oslo');
       //cy.get('p').contains("Norway");
     })
   })
