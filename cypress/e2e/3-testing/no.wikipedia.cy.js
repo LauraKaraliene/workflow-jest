c9f1f87dc5cc487decf930d2c267b83da7ff3a48
Laura Karaliene <Laura.Karaliene@stud.noroff.no>
2024-04-06 18:21:51 +0200

    changed no.wikipedia.org code

--- cypress/e2e/3-testing/no.wikipedia.cy.js
+++ cypress/e2e/3-testing/no.wikipedia.cy.js
@@ -1,9 +1,26 @@
-describe("en.wikipedia.org", () => {
-    it("can search for Oslo", () => {
+//describe("en.wikipedia.org", () => {
+   // it("can search for Oslo", () => {
+    //  cy.visit("https://en.wikipedia.org");
+    //  cy.get("input#searchInput").type("Oslo{enter}", { delay: 500 });
+    //  cy.url().should('include', '/wiki/Oslo');
+     // cy.get('h1').should('contain', 'Oslo');
+    //  //cy.get('p').contains("Norway");
+  //  })
+ // })
+
+ describe("en.wikipedia.org", () => {
+  it("can search for Oslo", () => {
       cy.visit("https://en.wikipedia.org");
-      cy.get("input#searchInput").type("Oslo{enter}", { delay: 500 });
-      cy.url().should('include', '/wiki/Oslo');
-      cy.get('h1').should('contain', 'Oslo');
-      //cy.get('p').contains("Norway");
-    })
-  })
+      // Target the input, type the search term
+      cy.get("input#searchInput").type("Oslo");
+      // Submit the form directly
+      //cy.get('form').submit();
+      cy.get('form').eq(0).submit(); // .eq(0) selects the first form
+
+      // Proceed with your assertions
+      cy.url({ timeout: 10000 }).should('include', '/wiki/Oslo');
+      cy.get('h1 > span').should('contain', 'Oslo');
+  });
+});
+
+
